c3c26b0fccc27ee9aa91530aec6c46d4a0071c1d
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-02-19 18:05:34 -0300

    feat: implement Dashboard Header component

---
+++ src/components/Dashboard/DashboardHeader.tsx
@@ -0,0 +1,47 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import { Heading } from "../Heading";
+import { DateTime } from "./DateTime";
+import { Weather } from "./Weather";
+
+import logo from "../../assets/logo-black.png";
+import arrow from "../../assets/arrow.png";
+
+import classes from "./DashboardHeader.module.css";
+
+export const DashboardHeader = () => {
+	const navigate = useNavigate();
+
+	const logoutHandler = () => {
+		navigate("/");
+	};
+
+	return (
+		<header className={classes.container}>
+			<div className={classes.heading}>
+				<Heading
+					title="Weekly Planner"
+					text="Use this planner to organize your daily issues."
+				/>
+			</div>
+
+			<DateTime />
+
+			<Weather />
+
+			<div className={classes.actions} onClick={logoutHandler}>
+				<Link to="https://compass.uol/en/home/" target="_black">
+					<img
+						className={classes.logo}
+						src={logo}
+						alt="Compass.uol logo"
+					/>
+				</Link>
+				<div className={classes.logout}>
+					<img className={classes.arrow} src={arrow} alt="Arrow" />
+					<p>Logout</p>
+				</div>
+			</div>
+		</header>
+	);
+};
