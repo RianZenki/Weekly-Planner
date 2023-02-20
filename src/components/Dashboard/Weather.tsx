943741172f28e0bdc9c1b5712e191486e4c78843
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-02-20 14:23:50 -0300

    refactor: change display weather temp

--- src/components/Dashboard/Weather.tsx
+++ src/components/Dashboard/Weather.tsx
@@ -1,42 +1,54 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import cloudy from "../../assets/cloudy.png";
 
 import classes from "./Weather.module.css";
 
 export const Weather = () => {
-	const data = useLoaderData();
+	const navigate = useNavigate();
+	const [weather, setWeather] = useState([]);
+
+	const storedUser = localStorage.getItem("user");
+
+	if (!storedUser) {
+		localStorage.removeItem("user");
+		localStorage.removeItem("authenticated");
+		navigate("/");
+	}
+
+	const userData = JSON.parse(storedUser!);
+
+	useEffect(() => {
+		const fetchWeather = async () => {
+			const city = userData.city;
+			const apiKey = "0da92eb9bfad0a0069368b83e2d5411d";
+			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt&appid=`;
+
+			const response = await fetch(url + apiKey);
+			const data = await response.json();
+			setWeather(data);
+		};
+		fetchWeather();
+	}, [userData.city]);
 
 	return (
 		<div className={classes["weather-container"]}>
-			<p className={classes.location}>São Paulo - Brasil</p>
+			<p className={classes.location}>
+				{userData.city} - {userData.country}
+			</p>
 
 			<>
-				{data && (
+				{weather && (
 					<div className={classes["temp-wrapper"]}>
 						<img src={cloudy} alt="cloudy" />
-						<p className={classes.temp}>20°</p>
-						{/* <p className={classes.temp}>{`${Math.round(data.main.temp)}°`}</p> */}
+						<p className={classes.temp}>22°</p>
+						{/* <p className={classes.temp}>{weather.main.temp}°</p> */}
 					</div>
 				)}
 
-				{!data && <p className={classes["error"]}>City not found</p>}
+				{!weather && <p className={classes["error"]}>City not found</p>}
 			</>
 		</div>
 	);
-};
-
-export const loader = async () => {
-	const city = "São Paulo";
-	const apiKey = "0da92eb9bfad0a0069368b83e2d5411d";
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt&appid=`;
-
-	const response = await fetch(url + apiKey);
-
-	if (!response.ok) {
-		console.log("Error");
-		return null;
-	}
-
-	return response;
 };
