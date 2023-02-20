7c1f1c0986c03999bc07882a1b08cf73f2ea4cfe
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-02-20 07:34:29 -0300

    feat: getting tasks information using context and rendering

--- src/components/Dashboard/Board.tsx
+++ src/components/Dashboard/Board.tsx
@@ -1,87 +1,35 @@
+import { useContext } from "react";
+
+import { WeekdayList } from "./Weekday/WeekdayList";
+import { TasksList } from "./Task/TasksList";
+import { TaskContext } from "../../store/task-context";
+
 import classes from "./Board.module.css";
 
 export const Board = () => {
+	const taskCtx = useContext(TaskContext);
+	const weekDayIndex = taskCtx.tasks.findIndex(
+		(item) => item.weekday === taskCtx.weekdaySelected
+	);
+	const weekDayListTasks = taskCtx.tasks[weekDayIndex].taskList;
+
+	weekDayListTasks.sort(function (a, b) {
+		if (a.time < b.time) {
+			return -1;
+		}
+		if (a.time > b.time) {
+			return 1;
+		}
+		return 0;
+	});
+
 	return (
 		<main className={classes["board-container"]}>
 			<div className={classes["weekday-wrapper"]}>
-				<ul className={classes["weekday-list"]}>
-					<li key="monday">Monday</li>
-					<li key="tuesday" className={classes.active}>
-						Tuesday
-					</li>
-					<li key="wednesday">Wednesday</li>
-					<li key="Thursday">Thursday</li>
-					<li key="friday">Friday</li>
-					<li key="saturday">Saturday</li>
-					<li key="sunday">Sunday</li>
-				</ul>
+				<WeekdayList />
 			</div>
 			<div className={classes.board}>
-				<div className={classes["time-wrapper"]}>
-					<ol className={classes["time-list"]}>
-						<li className={`${classes.card} ${classes.header}`}>Time</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-						<li className={`${classes.card} ${classes.item}`}>10h 30m</li>
-					</ol>
-				</div>
-				<div className={classes["tasks-wrapper"]}>
-					<div
-						className={`${classes["task-line"]} ${classes["conflict"]}`}
-					>
-						<ul className={classes["task-list"]}>
-							<li className={classes.conflict}>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing
-									elit Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit
-								</p>
-								<button>Delete</button>
-							</li>
-							<li className={classes.conflict}>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing
-									elit Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit
-								</p>
-								<button>Delete</button>
-							</li>
-							<li className={classes.conflict}>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing
-									elit Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit
-								</p>
-								<button>Delete</button>
-							</li>
-							<li className={classes.conflict}>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing
-									elit Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit
-								</p>
-								<button>Delete</button>
-							</li>
-						</ul>
-					</div>
-					<div className={classes["task-line"]}>
-						<ul className={classes["task-list"]}>
-							<li>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing
-									elit Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit
-								</p>
-								<button>Delete</button>
-							</li>
-						</ul>
-					</div>
-				</div>
+				<TasksList list={weekDayListTasks} />
 			</div>
 		</main>
 	);
