ab21e85ce54d4f8ff3b3865758ca4222d96c701c
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-02-20 13:42:38 -0300

    refactor: using useEffect hook instead loader to fetch data do external API

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Dashboard } from "./pages/Dashboard";
-import { loader as weatherLoader } from "./components/Dashboard/Weather";
 
 const router = createBrowserRouter([
 	{ path: "", element: <Login /> },
 	{ path: "register", element: <Register /> },
-	{ path: "dashboard", element: <Dashboard />, loader: weatherLoader },
+	{ path: "dashboard", element: <Dashboard /> },
 ]);
 
 function App() {
