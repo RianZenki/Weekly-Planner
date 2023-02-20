ac0a208f0bcf1028bee005f4f98dd1ed5ed9e154
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-02-20 13:50:27 -0300

    feat: implement protected route and adding styles to page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,30 +1,43 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { DashboardHeader } from "../components/Dashboard/DashboardHeader";
 import { DashboardAction } from "../components/Dashboard/DashboardAction";
 import { Board } from "../components/Dashboard/Board";
 
 import logoBackground from "../assets/logo-background.png";
 
+import classes from "./Dashboard.module.css";
+
+export const WEEKDAYS = [
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+	"Sunday",
+];
+
 export const Dashboard = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (
+			!localStorage.getItem("user") ||
+			!localStorage.getItem("authenticated")
+		) {
+			navigate("/");
+		}
+	}, [navigate]);
+
 	return (
-		<div
-			style={{
-				position: "relative",
-				height: "100vh",
-				backgroundColor: "rgba(255, 255, 255, .96)",
-				zIndex: 0,
-			}}
-		>
+		<div className={classes["dashboard-container"]}>
 			<DashboardHeader />
 			<DashboardAction />
 			<Board />
 			<img
-				style={{
-					width: 450,
-					position: "absolute",
-					right: 0,
-					bottom: 0,
-					zIndex: -1,
-				}}
+				className={classes["background-img"]}
 				src={logoBackground}
 				alt="logo"
 			/>
