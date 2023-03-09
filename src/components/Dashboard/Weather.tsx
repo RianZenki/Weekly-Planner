7c1c03c756cb161e5d891f916b14ba6d790f0203
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-03-09 16:29:14 -0300

    feat: implement typing for API return

--- src/components/Dashboard/Weather.tsx
+++ src/components/Dashboard/Weather.tsx
@@ -5,11 +5,26 @@ import cloudy from "../../assets/cloudy.png";
 
 import classes from "./Weather.module.css";
 
+type SuccessType = {
+	cod: number | string;
+	main?: {
+		temp: number;
+	};
+};
+
+type ErrorType = {
+	cod: string;
+};
+
+type WeatherType = SuccessType;
+
 export const Weather = () => {
 	const navigate = useNavigate();
-	const [weather, setWeather] = useState([]);
+	const [weather, setWeather] = useState<WeatherType>();
 
 	const storedUser = localStorage.getItem("user");
+
+	console.log(weather)
 
 	if (!storedUser) {
 		localStorage.removeItem("user");
@@ -21,7 +36,7 @@ export const Weather = () => {
 
 	useEffect(() => {
 		const fetchWeather = async () => {
-			const city = userData.city;
+			const city = userData.city || "";
 			const apiKey = "0da92eb9bfad0a0069368b83e2d5411d";
 			const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt&appid=`;
 
@@ -30,25 +45,26 @@ export const Weather = () => {
 			setWeather(data);
 		};
 		fetchWeather();
-	}, [userData.city]);
+	}, []);
 
 	return (
 		<div className={classes["weather-container"]}>
-			<p className={classes.location}>
-				{userData.city} - {userData.country}
-			</p>
+			{userData && (
+				<p className={classes.location}>
+					{userData.city} - {userData.country}
+				</p>
+			)}
 
-			<>
-				{weather && (
-					<div className={classes["temp-wrapper"]}>
-						<img src={cloudy} alt="cloudy" />
-						<p className={classes.temp}>22°</p>
-						{/* <p className={classes.temp}>{weather.main.temp}°</p> */}
-					</div>
-				)}
+			{weather && weather.cod === 200 && (
+				<div className={classes["temp-wrapper"]}>
+					<img src={cloudy} alt="cloudy" />
+					{/* <p className={classes.temp}>{Math.round(weather.main.temp)}°</p> */}
+				</div>
+			)}
 
-				{!weather && <p className={classes["error"]}>City not found</p>}
-			</>
+			{weather && weather.cod === "404" && (
+				<p className={classes["error"]}>City not found</p>
+			)}
 		</div>
 	);
 };
