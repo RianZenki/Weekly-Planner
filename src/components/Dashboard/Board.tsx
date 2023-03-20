9d655bd613883c3bc6b8271e4f3dea774afcbc35
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-03-20 07:40:21 -0300

    feat: get the task list by api request and manage it

--- src/components/Dashboard/Board.tsx
+++ src/components/Dashboard/Board.tsx
@@ -1,27 +1,78 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { WeekdayList } from "./Weekday/WeekdayList";
 import { TasksList } from "./Task/TasksList";
-import { TaskContext } from "../../store/task-context";
+import { UserContext } from "../../store/user-context";
+import { TaskType } from "../../types/task";
+import { baseUrl } from "../../utils/api";
 
 import classes from "./Board.module.css";
 
-export const Board = () => {
-	const taskCtx = useContext(TaskContext);
-	const weekDayIndex = taskCtx.tasks.findIndex(
-		(item) => item.weekday === taskCtx.weekdaySelected
-	);
-	const weekDayListTasks = taskCtx.tasks[weekDayIndex].taskList;
+type EventType = {
+	_id: string;
+	description: string;
+	dayOfWeek: string;
+	UserId: string;
+};
 
-	weekDayListTasks.sort(function (a, b) {
-		if (a.time < b.time) {
-			return -1;
+type FetchedEventsType = {
+	events: EventType[];
+};
+
+export const Board = () => {
+	const userCtx = useContext(UserContext);
+	const weekdaySelected = userCtx.weekdaySelected.toLocaleLowerCase();
+	const userToken = localStorage.getItem("token");
+	const [tasks, setTasks] = useState<any>();
+
+	const groupTasks = (tasksList: TaskType[]) => {
+		return tasksList.reduce((group: any, task) => {
+			const { time } = task;
+			group[time] = group[time] ?? [];
+			group[time].push(task);
+			return group;
+		}, {});
+	};
+
+	const transformTask = ({ events }: FetchedEventsType) => {
+		const transformedTasks = events.map((task: EventType) => {
+			const taskData = task.description.split(" - ");
+			const time = taskData[0];
+			const description = taskData[1];
+			return {
+				_id: task._id,
+				dayOfWeek: task.dayOfWeek,
+				description,
+				time,
+			};
+		});
+
+		return groupTasks(transformedTasks);
+	};
+
+	const fetchTasks = async () => {
+		const response = await fetch(
+			`${baseUrl}/events?dayOfWeek=${weekdaySelected}`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: "Bearer " + userToken,
+				},
+			}
+		);
+
+		const data = await response.json();
+
+		const groupedTasks = transformTask(data);
+		setTasks(groupedTasks);
+	};
+
+	useEffect(() => {
+		if (userToken) {
+			setTasks({});
+			fetchTasks();
 		}
-		if (a.time > b.time) {
-			return 1;
-		}
-		return 0;
-	});
+	}, [weekdaySelected, userToken]);
 
 	return (
 		<main className={classes["board-container"]}>
@@ -29,7 +80,7 @@ export const Board = () => {
 				<WeekdayList />
 			</div>
 			<div className={classes.board}>
-				<TasksList list={weekDayListTasks} />
+				<TasksList list={tasks} />
 			</div>
 		</main>
 	);
