bedc60e3ba09c5bd929a2462e26bdc6f4755bf28
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-03-20 13:07:49 -0300

    feat: implement error handling using alert component

--- src/components/Dashboard/DashboardAction.tsx
+++ src/components/Dashboard/DashboardAction.tsx
@@ -2,6 +2,7 @@ import { useContext, useRef, useState } from "react";
 import { Oval } from "react-loader-spinner";
 
 import { UserContext } from "../../store/user-context";
+import { Alert, AlertType } from "../Alert/Alert";
 import plus from "../../assets/plus.png";
 import minus from "../../assets/minus.png";
 import { WEEKDAYS } from "../../pages/Dashboard";
@@ -15,6 +16,8 @@ export const DashboardAction = () => {
 	const userToken = localStorage.getItem("token");
 	const [isAddLoading, setIsAddLoading] = useState<boolean>(false);
 	const [isDelLoading, setIsDelLoading] = useState<boolean>(false);
+	const [showAlert, setShowAlert] = useState<boolean>(false);
+	const [alertInfo, setAlertInfo] = useState<AlertType>();
 
 	const inputRef = useRef<HTMLInputElement>(null);
 	const weekdayRef = useRef<HTMLSelectElement>(null);
@@ -43,9 +46,27 @@ export const DashboardAction = () => {
 			}),
 		});
 
-		const data = await response.json();
+		if (!response.ok) {
+			setAlertInfo({
+				type: "warning",
+				description: "Error to create the task",
+			});
+			setShowAlert(true);
+			setTimeout(() => {
+				setShowAlert(false);
+			}, 3000);
+		}
 
-		console.log(data);
+		if (response.ok) {
+			setAlertInfo({
+				type: "success",
+				description: "Task created successfully",
+			});
+			setShowAlert(true);
+			setTimeout(() => {
+				setShowAlert(false);
+			}, 3000);
+		}
 
 		inputRef.current!.value = "";
 		setIsAddLoading(false);
@@ -64,106 +85,137 @@ export const DashboardAction = () => {
 			}
 		);
 
-		const data = await response.json();
-		console.log(data);
+		if (!response.ok) {
+			setAlertInfo({
+				type: "warning",
+				description: "Error to delete all tasks",
+			});
+			setShowAlert(true);
+			setTimeout(() => {
+				setShowAlert(false);
+			}, 3000);
+		}
+
+		if (response.ok) {
+			setAlertInfo({
+				type: "success",
+				description: "All tasks deleted successfully",
+			});
+			setShowAlert(true);
+			setTimeout(() => {
+				setShowAlert(false);
+			}, 3000);
+		}
 
 		setIsDelLoading(false);
 	};
 
 	return (
-		<div className={classes["dashboard-action-container"]}>
-			<form>
-				<div className={classes["inputs-wrapper"]}>
-					<input ref={inputRef} type="text" placeholder="Task or issue" />
-					<select ref={weekdayRef}>
-						{WEEKDAYS.map((day) => (
-							<option key={day} value={day}>
-								{day}
+		<>
+			{showAlert && (
+				<Alert
+					type={alertInfo?.type}
+					description={alertInfo?.description}
+				/>
+			)}
+			<div className={classes["dashboard-action-container"]}>
+				<form>
+					<div className={classes["inputs-wrapper"]}>
+						<input
+							ref={inputRef}
+							type="text"
+							placeholder="Task or issue"
+						/>
+						<select ref={weekdayRef}>
+							{WEEKDAYS.map((day) => (
+								<option key={day} value={day}>
+									{day}
+								</option>
+							))}
+						</select>
+
+						<select ref={timeRef}>
+							<option key={"10h 30m"} value={"10h 30m"}>
+								10h 30m
 							</option>
-						))}
-					</select>
+							<option key={"11h 00m"} value={"11h 00m"}>
+								11h 00m
+							</option>
+							<option key={"13h 10m"} value={"13h 10m"}>
+								13h 10m
+							</option>
+							<option key={"15h 00m"} value={"15h 00m"}>
+								15h 00m
+							</option>
+							<option key={"16h 55m"} value={"16h 55m"}>
+								16h 55m
+							</option>
+							<option key={"17h 25m"} value={"17h 25m"}>
+								17h 25m
+							</option>
+							<option key={"22h 15m"} value={"22h 15m"}>
+								22h 15m
+							</option>
+						</select>
+					</div>
 
-					<select ref={timeRef}>
-						<option key={"10h 30m"} value={"10h 30m"}>
-							10h 30m
-						</option>
-						<option key={"11h 00m"} value={"11h 00m"}>
-							11h 00m
-						</option>
-						<option key={"13h 10m"} value={"13h 10m"}>
-							13h 10m
-						</option>
-						<option key={"15h 00m"} value={"15h 00m"}>
-							15h 00m
-						</option>
-						<option key={"16h 55m"} value={"16h 55m"}>
-							16h 55m
-						</option>
-						<option key={"17h 25m"} value={"17h 25m"}>
-							17h 25m
-						</option>
-						<option key={"22h 15m"} value={"22h 15m"}>
-							22h 15m
-						</option>
-					</select>
-				</div>
+					<div className={classes["buttons-wrapper"]}>
+						<button
+							type="button"
+							className={classes.add}
+							onClick={addNewTaskHandler}
+						>
+							{!isAddLoading && (
+								<>
+									<img src={plus} alt={plus} />
+									Add to Calendar
+								</>
+							)}
 
-				<div className={classes["buttons-wrapper"]}>
-					<button
-						type="button"
-						className={classes.add}
-						onClick={addNewTaskHandler}
-					>
-						{!isAddLoading && (
-							<>
-								<img src={plus} alt={plus} />
-								Add to Calendar
-							</>
-						)}
+							{isAddLoading && (
+								<Oval
+									height={20}
+									width={20}
+									color="#ffffff"
+									visible={true}
+									ariaLabel="oval-loading"
+									secondaryColor="#ffffff"
+									strokeWidth={5}
+									strokeWidthSecondary={5}
+								/>
+							)}
+							{isAddLoading && "Adding..."}
+						</button>
 
-						{isAddLoading && (
-							<Oval
-								height={20}
-								width={20}
-								color="#ffffff"
-								visible={true}
-								ariaLabel="oval-loading"
-								secondaryColor="#ffffff"
-								strokeWidth={5}
-								strokeWidthSecondary={5}
-							/>
-						)}
-						{isAddLoading && "Adding..."}
-					</button>
+						<button
+							className={classes.delete}
+							type="button"
+							onClick={deleteAllTasksHandler}
+						>
+							{!isDelLoading && (
+								<>
+									<img src={minus} alt={minus} />
+									Delete All
+								</>
+							)}
 
-					<button
-						className={classes.delete}
-						type="button"
-						onClick={deleteAllTasksHandler}
-					>
-						{!isDelLoading && (
-							<>
-								<img src={minus} alt={minus} />
-								Delete All
-							</>
-						)}
-
-						{isDelLoading && (
-							<Oval
-								height={20}
-								width={20}
-								color="#ffffff"
-								visible={true}
-								ariaLabel="oval-loading"
-								secondaryColor="#ffffff"
-								strokeWidth={5}
-								strokeWidthSecondary={5}
-							/>
-						)}
-						{isDelLoading && "Deleting..."}
-					</button>
-				</div>
-			</form>
-		</div>
+							{isDelLoading && (
+								<Oval
+									height={20}
+									width={20}
+									color="#ffffff"
+									visible={true}
+									ariaLabel="oval-loading"
+									secondaryColor="#ffffff"
+									strokeWidth={5}
+									strokeWidthSecondary={5}
+								/>
+							)}
+							{isDelLoading && "Deleting..."}
+						</button>
+					</div>
+				</form>
+			</div>
+		</>
 	);
 };
