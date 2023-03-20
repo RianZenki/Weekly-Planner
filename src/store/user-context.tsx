fc062e96357576ef02d02882a7e433c7ae2fab6b
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-03-20 12:04:47 -0300

    refactor: change context api, remove user infos

--- src/store/user-context.tsx
+++ src/store/user-context.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 
-type UserType = {
-	city: string;
-};
-
 type userContextType = {
 	weekdaySelected: string;
-	token: string;
-	user?: UserType;
 	onWeekdayChange: (weekday: string) => void;
-	onStoreUserInfo: ({
-		token,
-		user,
-	}: {
-		token: string;
-		user: UserType;
-	}) => void;
 };
 
 export const UserContext = React.createContext<userContextType>({
 	weekdaySelected: "Monday",
 	onWeekdayChange: (weekday: string) => {},
-	token: "",
-	user: { city: "" },
-	onStoreUserInfo: ({ token, user }: { token: string; user: UserType }) => {},
 });
